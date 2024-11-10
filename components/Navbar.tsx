41051c8a8e6a540bd8730860a1db70dee27585eb
JC Ejercito <joshejercito01@gmail.com>
2024-11-10 20:58:10 +0800

    update: navbar fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../styles/Navbar.module.css';
 
 interface NavbarProps {
-  balance: number; // balance prop to pass remaining balance
+  balance: number;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ balance }) => {
@@ -20,7 +20,7 @@ const Navbar: React.FC<NavbarProps> = ({ balance }) => {
 
       {/* Wallet icon, user icon, and balance */}
       <div className={styles.navbar__icons}>
-        <div className={`${styles.navbar__icon} ${styles.navbar__walletIcon}`}>💰</div>
+        <div className={`${styles.navbar__icon} ${styles.navbar__walletIcon}`}>👛</div>
         <div className={styles.navbar__balance}>${balance.toFixed(2)}</div>
         <p>|</p>
         <div className={`${styles.navbar__icon} ${styles.navbar__userIcon}`}>👤</div>
